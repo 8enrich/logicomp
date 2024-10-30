a3f8d0ae102d965e376239e8b3cc6f6a795a40f1
henriqueftcunha@gmail.com <henriqueftcunha@gmail.com>
2024-10-29 22:22:49 -0300

    Fazendo as funções da lista 1

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
 do some computation on its syntactic structure. """
 
 
-from formula import *
+from formula import * 
 
 
 def length(formula: Formula):
@@ -96,43 +96,65 @@ def is_literal(formula: Formula):
     if isinstance(formula, Atom):
         return True
     if isinstance(formula, Not):
-        return is_literal(formula)
+        return is_literal(formula.inner)
     if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
         return False
 
 def substitution(formula: Formula, old_subformula: Formula, new_subformula: Formula):
     """Returns a new formula obtained by replacing all occurrences
     of old_subformula in the input formula by new_subformula."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
-
+    if isinstance(formula, Not):
+        formula.inner = substitution(formula.inner, old_subformula, new_subformula)
+    if isinstance(formula, (Or, Implies, And)):
+        formula.right = substitution(formula.right, old_subformula, new_subformula)
+        formula.left = substitution(formula.left, old_subformula, new_subformula)
+    if formula == old_subformula:
+        formula = new_subformula
+    return formula
 
 def is_clause(formula: Formula):
     """Returns True if formula is a clause. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
-
+    if isinstance(formula, (Atom, Not)):
+        return is_literal(formula)
+    if isinstance(formula, Or):
+        return is_clause(formula.left) and is_clause(formula.right)
+    if isinstance(formula, (Implies, And)):
+        return False
 
 def is_negation_normal_form(formula: Formula):
     """Returns True if formula is in negation normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
-
+    if isinstance(formula, Atom):
+        return True
+    if isinstance(formula, Not):
+        return is_negation_normal_form(formula.inner)
+    if isinstance(formula, Implies):
+        return False
+    if isinstance(formula, (And, Or)):
+        return is_negation_normal_form(formula.left) and is_negation_normal_form(formula.right)
 
 def is_cnf(formula: Formula):
     """Returns True if formula is in conjunctive normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
-
+    if isinstance(formula, (Atom, Not, Implies, Or)):
+        return is_clause(formula)
+    if isinstance(formula, And):
+        return is_cnf(formula.left) and is_cnf(formula.right)
 
 def is_term(formula: Formula):
     """Returns True if formula is a term. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
-
+    if isinstance(formula, (Atom, Not, Implies, Or)):
+        return is_literal(formula)
+    if isinstance(formula, And):
+        return is_term(formula.left) and is_term(formula.right)
 
 def is_dnf(formula: Formula):
     """Returns True if formula is in disjunctive normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
-
+    if isinstance(formula, (Atom, Not, Implies, And)):
+        return is_term(formula)
+    if isinstance(formula, Or):
+        return is_dnf(formula.left) and is_dnf(formula.right)
 
 def is_decomposable_negation_normal_form(formula: Formula):
     """Returns True if formula is in decomposable negation normal form.
