9618c45dce836313ad5a16673f0ec68f69a1a32a
8enrich <henriqueftcunha@gmail.com>
2025-01-15 11:29:13 -0300

    Melhorando o tseytin

---
+++ dpll.py
@@ -0,0 +1,107 @@
+from formula import Atom, Not, Or, Implies, And
+from functions import is_cnf
+from functools import reduce
+
+def remove_implies(formula):
+    if isinstance(formula, Not):
+        return Not(remove_implies(formula.inner))
+    if isinstance(formula, And):
+        return And(remove_implies(formula.left), remove_implies(formula.right))
+    if isinstance(formula, Or):
+        return Or(remove_implies(formula.left), remove_implies(formula.right))
+    if isinstance(formula, Implies):
+        return Or(Not(remove_implies(formula.left)), remove_implies(formula.right))
+    return formula
+
+def de_morgan(formula):
+    if isinstance(formula, Not):
+        if isinstance(formula.inner, Or):
+            return And(Not(de_morgan(formula.inner.left)), Not(de_morgan(formula.inner.right)))
+        if isinstance(formula.inner, And):
+            return Or(Not(de_morgan(formula.inner.left)), Not(de_morgan(formula.inner.right)))
+        return Not(de_morgan(formula.inner))
+    if isinstance(formula, And):
+        return And(de_morgan(formula.left), de_morgan(formula.right))
+    if isinstance(formula, Or):
+        return Or(de_morgan(formula.left), de_morgan(formula.right))
+    return formula
+
+def remove_double_negation(formula):
+    if isinstance(formula, Not):
+        if isinstance(formula.inner, Not):
+            return remove_double_negation(formula.inner.inner)
+        return Not(remove_double_negation(formula.inner))
+    if isinstance(formula, And):
+        return And(remove_double_negation(formula.left), remove_double_negation(formula.right))
+    if isinstance(formula, Or):
+        return Or(remove_double_negation(formula.left), remove_double_negation(formula.right))
+    return formula
+
+def or_distribuctive(formula):
+    if isinstance(formula, Or):
+        if isinstance(formula.left, And):
+            return And(
+                Or(or_distribuctive(formula.left.left), or_distribuctive(formula.right)),
+                Or(or_distribuctive(formula.left.right), or_distribuctive(formula.right))
+            )
+        if isinstance(formula.right, And):
+            return And(
+                Or(or_distribuctive(formula.left), or_distribuctive(formula.right.left)),
+                Or(or_distribuctive(formula.left), or_distribuctive(formula.right.right))
+            )
+        return Or(or_distribuctive(formula.left), or_distribuctive(formula.right))
+    if isinstance(formula, And):
+        return And(or_distribuctive(formula.left), or_distribuctive(formula.right))
+    return formula
+
+def nnf_transform(formula):
+    formula = remove_implies(formula)
+    while True:
+        last = formula
+        formula = de_morgan(formula)
+        if formula == last:
+            break
+    return formula
+
+def cnf_direct_transform(formula):
+    formula = nnf_transform(formula)
+    formula = remove_double_negation(formula)
+    while not is_cnf(formula):
+        formula = or_distribuctive(formula)
+    return formula
+
+def cnf_tseitin_transform(formula):
+    formula = nnf_transform(formula)
+    formula = remove_double_negation(formula)
+    new_vals = {}
+    clauses = set() 
+    num = 1
+    def get_clauses(formula):
+        nonlocal num
+        if isinstance(formula, Atom):
+            return formula
+        new_val = new_vals.get(formula)
+        if not new_val:
+            new_vals[formula] = Atom(f'p_{num}')
+            num += 1
+        new_val = new_vals.get(formula)
+        if isinstance(formula, Not):
+            b = get_clauses(formula.inner)
+            clauses.add(Or(Not(new_val), Not(b)))
+            clauses.add(Or(b, new_val))
+        if isinstance(formula, (And, Or)):
+            left = get_clauses(formula.left)
+            right = get_clauses(formula.right)
+            if isinstance(formula, And):
+                clauses.add(Or(Not(new_val), left))
+                clauses.add(Or(Not(new_val), right))
+                clauses.add(Or(new_val, Or(Not(left), Not(right))))
+            if isinstance(formula, Or):
+                clauses.add(Or(new_val, Not(left)))
+                clauses.add(Or(new_val, Not(right)))
+                clauses.add(Or(Not(new_val), Or(left, right)))
+        return new_val
+    get_clauses(formula)
+    new_formula = new_vals[formula]
+    new_formula = reduce(lambda x, y: And(x, y), clauses)
+    return new_formula 
--- semantics.py
+++ semantics.py
@@ -1,11 +1,7 @@
 """The goal in this module is to define functions associated with the semantics of formulas in propositional logic. """
 
-
 from formula import *
-from functions import atoms, is_cnf, subformulas, is_literal
 from itertools import product
-from functools import reduce
-
 
 def truth_value(formula, interpretation):
     """Determines the truth value of a formula in an interpretation (valuation).
@@ -64,88 +60,3 @@ def truth_table(formula):
         row = {atom: value for atom, value in zip(list_atoms, v)}
         row[formula] = truth_value(formula, row)
         yield row
-
-def remove_implies(formula):
-    if isinstance(formula, Not):
-        return Not(remove_implies(formula.inner))
-    if isinstance(formula, And):
-        return And(remove_implies(formula.left), remove_implies(formula.right))
-    if isinstance(formula, Or):
-        return Or(remove_implies(formula.left), remove_implies(formula.right))
-    if isinstance(formula, Implies):
-        return Or(Not(remove_implies(formula.left)), remove_implies(formula.right))
-    return formula
-
-def de_morgan(formula):
-    if isinstance(formula, Not):
-        if isinstance(formula.inner, Or):
-            return And(Not(de_morgan(formula.inner.left)), Not(de_morgan(formula.inner.right)))
-        if isinstance(formula.inner, And):
-            return Or(Not(de_morgan(formula.inner.left)), Not(de_morgan(formula.inner.right)))
-        return Not(de_morgan(formula.inner))
-    if isinstance(formula, And):
-        return And(de_morgan(formula.left), de_morgan(formula.right))
-    if isinstance(formula, Or):
-        return Or(de_morgan(formula.left), de_morgan(formula.right))
-    return formula
-
-def remove_double_negation(formula):
-    if isinstance(formula, Not):
-        if isinstance(formula.inner, Not):
-            return remove_double_negation(formula.inner.inner)
-        return Not(remove_double_negation(formula.inner))
-    if isinstance(formula, And):
-        return And(remove_double_negation(formula.left), remove_double_negation(formula.right))
-    if isinstance(formula, Or):
-        return Or(remove_double_negation(formula.left), remove_double_negation(formula.right))
-    return formula
-
-def or_distribuctive(formula):
-    if isinstance(formula, Or):
-        if isinstance(formula.left, And):
-            return And(
-                Or(or_distribuctive(formula.left.left), or_distribuctive(formula.right)),
-                Or(or_distribuctive(formula.left.right), or_distribuctive(formula.right))
-            )
-        if isinstance(formula.right, And):
-            return And(
-                Or(or_distribuctive(formula.left), or_distribuctive(formula.right.left)),
-                Or(or_distribuctive(formula.left), or_distribuctive(formula.right.right))
-            )
-        return Or(or_distribuctive(formula.left), or_distribuctive(formula.right))
-    if isinstance(formula, And):
-        return And(or_distribuctive(formula.left), or_distribuctive(formula.right))
-    return formula
-
-def cnf_direct_transform(formula):
-    formula = remove_implies(formula)
-    while True:
-        last = formula
-        formula = de_morgan(formula)
-        if formula == last:
-            break
-    formula = remove_double_negation(formula)
-    while not is_cnf(formula):
-        formula = or_distribuctive(formula)
-    return formula
-
-def get_literals(formula):
-    if isinstance(formula, Not):
-        return [Not(get_literals(formula.inner)[0])]
-    if isinstance(formula, (And, Or, Implies)):
-        return get_literals(formula.left) + get_literals(formula.right)
-    if is_literal(formula):
-        return [formula]
-    return []
-
-def cnf_tseitin_transform(formula):
-    subforms = subformulas(formula)
-    subforms_not_atoms = list(filter(lambda x: not isinstance(x, Atom), subforms))
-    for i, subform in enumerate(subforms_not_atoms):
-        val = Atom(f'x{i + 1}')
-        literals = get_literals(subform)
-        ors = Or(Not(val), reduce(lambda x, y: Or(x, y), literals))
-        formula = And(formula, ors) if i != 0 else ors
-        for literal in literals:
-            formula = And(formula, Or(Not(literal), val))
-    return cnf_direct_transform(formula)
